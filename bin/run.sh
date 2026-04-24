dea8107d5ff46198f4a04713ceebda4171361c21
aschuma <aschuma@users.noreply.github.com>
2026-04-24 21:11:45 +0200

    feat: rename hostshare to workspace

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/shared-env.sh"
 
-tart run --no-graphics --dir=hostshare:"$HOST_SHARE_DIR" "$HOST" &
+tart run --no-graphics --dir=workspace:"$HOST_SHARE_DIR" "$HOST" &
